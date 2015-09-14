7e9fe0095909cfeadcabc5d4153dd863d8bf5c60
JackyShen <mebusw@163.com>
2015-09-14 22:56:10 +0800

    add line parser for test case steps

--- rflint.py
+++ rflint.py
@@ -1,3 +1,6 @@
+import re
+
+
 class RFLint(object):
     """docstring for RFLint"""
     
@@ -7,7 +10,11 @@ class RFLint(object):
         super(RFLint, self).__init__()
         self.filename = filename
         self.stat = {'line_count': 0, 'empty_line_count': 0, 'comment_line_count':0, 'row_count':0, 'section_count': 0}
+        self.testcases = {}
+        self.curr_testcase = None
 
+    def rule(self, element, NotWithInLayer=None, ShouldNotUse=None, WillResultIn=None):
+        pass
 
     def check(self):
         with open(self.filename) as f:
@@ -22,12 +29,18 @@ class RFLint(object):
                     self.section_type = line[3:].lstrip()[0]
                     self.on_section(line, self.section_type)
                 else:
+                    self.stat['row_count'] += 1
                     self.on_row(line)
 
     def on_section(self, line, section_type):
         print line, section_type
         self.stat['section_count'] += 1
 
-    def on_row(self, line):
-        print line
-        self.stat['row_count'] += 1
+    def on_row(self, row):
+        _space_splitter = re.compile(' {2,}|\t|\n')
+        column = _space_splitter.split(row)
+        if self.curr_testcase is None:
+            self.curr_testcase = column[0]
+            self.testcases[self.curr_testcase] = []
+        else:
+            self.testcases[self.curr_testcase].append(filter(lambda x: x and True, column))
--- rflintTest.py
+++ rflintTest.py
@@ -1,6 +1,26 @@
 import unittest
 from rflint import RFLint
 
+###
+# analyze-map_func()
+# DSL
+# reader - subclasses
+# generate tokens
+# extensions/processor/mixin
+# def _state_global(self, token):
+#         if token in ("class", "namespace"):
+#             self._state = self._state_namespace_def
+
+#   for token in tokens:
+#         reader.state(token)
+#         yield token
+#     reader.eof()
+    
+#   def state(self, token):
+#         self._state(token)
+# OutputScheme
+# whitelist_filter   any()
+
 class RFLintTest(unittest.TestCase):
     def setUp(self):
         pass
@@ -10,15 +30,17 @@ class RFLintTest(unittest.TestCase):
 
     def test_open_and_read_lines(self):
         lint = RFLint('sample.txt')
-
+        lint.rule('AnyKeyword', NotWithInLayer='ElementInteraction', ShouldNotUse='Selenium2LibKeywords', WillResultIn='ERROR')
+        
         lint.check()
 
-        self.assertEqual(7, lint.stat['line_count'])
+        self.assertEqual(8, lint.stat['line_count'])
         self.assertEqual(2, lint.stat['comment_line_count'])
         self.assertEqual(1, lint.stat['empty_line_count'])
         self.assertEqual(2, lint.stat['section_count'])
-        self.assertEqual(2, lint.stat['row_count'])
+        self.assertEqual(3, lint.stat['row_count'])
         # self.assertEqual(2, event_count)
+        print lint.testcases
 
     def test_read_line_events(self):
         pass
